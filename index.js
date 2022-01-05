1e47e94f9795ef8ee49b6e78233cd25f696ab21f
Basudev <murabasudev@gmail.com>
2022-01-05 20:00:08 +0530

    Added Cors

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ require("express-async-errors");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
 const productRouter = require("./routes/product");
+const payRouter = require("./routes/stripe");
 // const cartRouter = require("./routes/cart");
 // const orderRouter = require("./routes/order");
 //connect db
@@ -15,12 +17,15 @@ const authenticateUser = require("./middleware/authentication");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(cors());
+
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 // app.use("/api/v1/cart", cartRouter);
 // app.use("/api/v1/orders", orderRouter);
+app.use("/checkout", payRouter);
 
 //using middleware
 app.use(notFoundMiddleware);
