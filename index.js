aa8dffa039bf7ea0672e9a61e43b47b75a8586ec
Basudev <murabasudev@gmail.com>
2022-01-05 21:22:49 +0530

    Order and cart issue fixed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
 const productRouter = require("./routes/product");
 const payRouter = require("./routes/stripe");
-// const cartRouter = require("./routes/cart");
+const cartRouter = require("./routes/cart");
 const orderRouter = require("./routes/order");
 //connect db
 const connectDB = require("./db/connect");
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
-// app.use("/api/v1/cart", cartRouter);
+app.use("/api/v1/cart", cartRouter);
 app.use("/api/v1/orders", orderRouter);
 app.use("/checkout", payRouter);
 
