343880ab4e4789e289ecc2b8717f932bcc1c7d02
Basudev <murabasudev@gmail.com>
2022-01-07 19:22:48 +0530

    Made Some Changes while trying to use Payment gateway

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/cart", cartRouter);
 app.use("/api/v1/orders", orderRouter);
-app.use("/checkout", payRouter);
+app.use("/api/v1/checkout", payRouter);
 
 //using middleware
 app.use(notFoundMiddleware);
--- routes/stripe.js
+++ routes/stripe.js
@@ -2,20 +2,51 @@ const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.post("/payment", (req, res) => {
-  stripe.charges.create(
-    {
-      source: req.body.token.id,
-      amount: req.body.amount,
-      currency: "usd",
-    },
-    (stripeErr, stripeRes) => {
-      if (stripeErr) {
-        res.status(500).send(stripeErr);
-      } else {
-        res.status(200).send(stripeRes);
-      }
-    }
-  );
+  // stripe.charges.create(
+  //   {
+  //     source: req.body.tokenId,
+  //     amount: req.body.amount,
+  //     currency: "inr",
+  //   },
+  //   (stripeErr, stripeRes) => {
+  //     if (stripeErr) {
+  //       res.status(500).send(stripeErr);
+  //     } else {
+  //       res.status(200).send(stripeRes);
+  //     }
+  //   }
+  // );
+
+  // Stripe Official Docs
+  // Moreover you can take more details from user
+  // like Address, Name, etc from form
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken,
+      name: "Dev.to User",
+      address: {
+        line1: "TC 9/4 Old MES colony",
+        postal_code: "110092",
+        city: "New Delhi",
+        state: "Delhi",
+        country: "India",
+      },
+    })
+    .then((customer) => {
+      return stripe.charges.create({
+        amount: 7000, // Charing Rs 25
+        description: "Web Development Product",
+        currency: "USD",
+        customer: customer.id,
+      });
+    })
+    .then((charge) => {
+      res.send("Success"); // If no error occurs
+    })
+    .catch((err) => {
+      res.send(err); // If some error occurs
+    });
 });
 
 module.exports = router;
